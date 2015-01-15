6b37297f51a039bbd05cb17658f560d12099ef07
mirtlbu <bubuzoglo@gmail.com>
2015-01-15 23:27:35 +0300

    wip

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ $(window).load(function() {
     $('.flexslider').flexslider(
         {
             animation: 'slide',
-            itemWidth: '1170'
+            itemWidth: '1170',
+            slideshow: false,
+            controlNav: false,
+            animationSpeed: 1000,
+            slideshowSpeed: 8000
         }
     );
 });
