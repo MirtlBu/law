1ba3358ff9c33a263a8bc34451ae73366d15e81d
mirtlbu <bubuzoglo@gmail.com>
2015-01-16 17:14:11 +0300

    finished html and css

--- js/script.js
+++ js/script.js
@@ -10,3 +10,25 @@ $(window).load(function() {
         }
     );
 });
+
+
+ymaps.ready(init);
+var myMap, myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 14
+    });
+    myPlacemark = new ymaps.Placemark([55.76, 37.64],
+        { content: 'Москва!', balloonContent: 'ул. Старая, 34/2' },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icon-pin.png',
+            iconImageSize: [40, 40],
+            iconImageOffset: [-3, -42]
+        });
+    myMap.geoObjects.add(myPlacemark);
+    myMap.controls.remove('typeSelector').remove('mapTools').remove('searchControl').remove('trafficControl');
+
+}
